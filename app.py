2dcd54df2a39b3baa019b1be5f10f614934c413b
terrenceme15 <terrence.manlyelliott@gmail.com>
2024-03-30 15:38:40 -0400

    Added the SGD classifier

--- app.py
+++ app.py
@@ -8,10 +8,14 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from email import message_from_string
+import warnings
 import re
+
+warnings.filterwarnings('ignore')
 
 lgbm_model = joblib.load("lgbm_model.pkl")
 sgd_model = joblib.load("sgdClassifier.pkl")
+
 
 #Function to convert MIME to plain text
 def mime_to_text(mime_text):
@@ -55,15 +59,12 @@ def html_to_text(text):
   return soup.get_text()
 
 
-
-
 def main():
     st.title("Email Analysis")
     
-    # Text input for email
+    #Text input for email
     email = st.text_area("Enter email here", key="email_input")
-    
-    # Button to analyze email
+    #Button to analyze email
     if st.button("Analyze"):
         # Perform analysis and calculate spam percentage
         spam_percentage = analyze_email(email)
@@ -72,7 +73,7 @@ def main():
         st.write(f"Spam Percentage: {spam_percentage}%")
 
 def analyze_email(email):
-    #lightgbm_text = preprocess(email)
+    lightgbm_text = preprocess(email)
     #lgbm_prediction = lgbm_model.predict(lightgbm_text) * 100
 
     sgd_text = preprocess_email_sgd_logReg(email)
@@ -97,39 +98,38 @@ def preprocess_email_lightgbm(email):
     
     vectorizer = TfidfVectorizer(max_features=14804, stop_words='english')
     email_array = [email]
-    lightgbm_text = vectorizer.fit_transform(email_array).toarray()
+    lightgbm_text = vectorizer.transform(email_array)
 
     return lightgbm_text
 
 def preprocess_email_sgd_logReg(email):
     #Encoding the features column
-    vectorizer = TfidfVectorizer(max_features= 30000, stop_words='english')
-    sgd_logReg_text = vectorizer.fit_transform([email,]).toarray()
+    vectorizer = joblib.load('vectorizer.joblib')
+    sgd_logReg_text = vectorizer.transform([email])
     return sgd_logReg_text
 
 if __name__ == "__main__":
     main()
 
 
-'''
+
 # Load the models
-    lgbm_model = joblib.load("lgbm_model.pkl")
-    logistic_regression_model = joblib.load("logisticRegression.pkl")
-    #nn_model = keras.models.load_model("nn_model.keras")
-    sgd_classifier_model = joblib.load("sgdClassifier.pkl")
-    
-    # Preprocess the email
-    preprocessed_email = preprocess_email(email)
-    
-    # Make predictions using the models
-    lgbm_prediction = (lgbm_model.predict([preprocessed_email])[0]) * 100
-    logistic_regression_prediction = (logistic_regression_model.predict([preprocessed_email])[0])* 100
-    # nn_prediction = (nn_model.predict([preprocessed_email])[0]) * 100
-    sgd_classifier_prediction = (sgd_classifier_model.predict([preprocessed_email])[0]) * 100
-    
-    # Calculate the average spam probability
-    spam_probability = (lgbm_prediction + logistic_regression_prediction + 0 + sgd_classifier_prediction) / 4
-    
-    # Convert spam probability to percentage
-    spam_percentage = spam_probability 
-'''
+#    lgbm_model = joblib.load("lgbm_model.pkl")
+#    logistic_regression_model = joblib.load("logisticRegression.pkl")
+#    #nn_model = keras.models.load_model("nn_model.keras")
+#    sgd_classifier_model = joblib.load("sgdClassifier.pkl")
+#    
+#    # Preprocess the email
+#    preprocessed_email = preprocess_email(email)
+#    
+#    # Make predictions using the models
+#    lgbm_prediction = (lgbm_model.predict([preprocessed_email])[0]) * 100
+#    logistic_regression_prediction = (logistic_regression_model.predict([preprocessed_email])[0])* 100
+#    # nn_prediction = (nn_model.predict([preprocessed_email])[0]) * 100
+#    sgd_classifier_prediction = (sgd_classifier_model.predict([preprocessed_email])[0]) * 100
+#    
+#    # Calculate the average spam probability
+#    spam_probability = (lgbm_prediction + logistic_regression_prediction + 0 + sgd_classifier_prediction) / 4
+#    
+#    # Convert spam probability to percentage
+#    spam_percentage = spam_probability 
