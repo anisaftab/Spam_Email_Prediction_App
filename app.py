5d3bfdcf29994d5af022570d6e98239a520fbb31
terrenceme15 <terrence.manlyelliott@gmail.com>
2024-04-11 23:25:43 -0400

    Remodeled Application

--- app.py
+++ app.py
@@ -8,64 +8,20 @@ from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
 from email import message_from_string
+import base64
+import quopri
 import warnings
 import re
 
 warnings.filterwarnings('ignore')
 
-log_reg_model = joblib.load("log_reg_model.pkl")
-sgd_model = joblib.load("sgdClassifier.pkl")
 lgbm_model = joblib.load('lgbm_model.pkl')
 
-phish_vectorizer = joblib.load('phish_vectorizer.joblib')
+logistic_regression_model = joblib.load('logistic_regression_model.pkl')
+sgd_classifier = joblib.load('sgd_classifier.pkl')
+
 spam_vectorizer = joblib.load('spam_vectorizer.joblib')
-nn_tokenizer = joblib.load('nn_tokenizer.joblib')
-
-def custom_initializer(shape, dtype=None):
-    return tf.random.normal(shape, mean=0.0, stddev=0.1, dtype=dtype)
-nn_model = load_model("nn_model.keras", custom_objects={'custom_initializer': custom_initializer})
-
-
-#Function to convert MIME to plain text
-def mime_to_text(mime_text):
-  #Parse the MIME content
-  email_message = message_from_string(mime_text)
-  text_content = ""
-
-  #Check if the email is multipart
-  if email_message.is_multipart():
-      for part in email_message.walk():
-          content_type = part.get_content_type()
-          content_disposition = str(part.get("Content-Disposition"))
-
-          #Extract plain text
-          if content_type == "text/plain" and "attachment" not in content_disposition:
-              text = part.get_payload(decode=True)
-              try:
-                  text_content += text.decode('utf-8')
-              except UnicodeDecodeError:
-                  text_content += text.decode('utf-8', errors='replace')
-          #Convert HTML parts to text
-          elif content_type == "text/html" and "attachment" not in content_disposition:
-              html = part.get_payload(decode=True)
-              try:
-                  soup = BeautifulSoup(html.decode('utf-8'), "html.parser")
-                  text_content += soup.get_text()
-              except UnicodeDecodeError:
-                  soup = BeautifulSoup(html.decode('utf-8', errors='replace'), "html.parser")
-                  text_content += soup.get_text()
-  else:
-      #Return text if not multipart
-      try:
-          text_content = email_message.get_payload(decode=True).decode('utf-8')
-      except UnicodeDecodeError:
-          text_content = email_message.get_payload(decode=True).decode('utf-8', errors='replace')
-  return text_content
-
-#function to convert html to plain text
-def html_to_text(text):
-  soup = BeautifulSoup(text, 'lxml')
-  return soup.get_text()
+phish_vectorizer = joblib.load('phish_vectorizer.joblib')
 
 
 def main():
@@ -78,51 +34,102 @@ def main():
 
     #Text input for email
     email = st.text_area("Enter email here", key='email_input', value=st.session_state.email_input)
+
     #Button to analyze email
     if st.button("Analyze"):
         # Perform analysis and calculate spam percentage
-        phish_percentage = phish_analyze(email)
         spam_percentage = spam_analyze(email)
+        phish_percentage = phish_analyze(email)
+
+        spam_flag = None
+        phish_flag = None
+
+        if spam_percentage == 0:
+            spam_flag = False 
+        else: spam_flag = True
+
+
+        if phish_percentage == 0:
+            phish_flag = False 
+        else: phish_flag = True
 
         # Display phishing and spam percentages
-        #st.write(f"Phishing Email Chance: {phish_percentage}%")
-        st.write(f"Spam Email Chance: {spam_percentage}%")
-
-
-
-def phish_analyze(email):
-    text = preprocess(email)
-    text = phish_vectorizer.transform([text])
-
-    log_reg_prediction = log_reg_model.predict(text) * 100
-    #sgd_prediction = sgd_model.predict(text) * 100
-    average = log_reg_prediction
-    return  int(average)
+        st.write("Spam Email:", spam_flag)
+        st.write("Phishing Email:", phish_flag)
 
 
 def spam_analyze(email):
     text = preprocess(email)
-
+    print(text)
     spam_text = spam_vectorizer.transform([text])
-
-    nn_text_tokenized = nn_tokenizer.texts_to_sequences([text])
-    nn_text_padded = pad_sequences(nn_text_tokenized, maxlen=14804 )
-
-    nn_prediction = nn_model.predict(nn_text_padded) * 100
     lgbm_prediction = lgbm_model.predict(spam_text) * 100
-    print(nn_prediction)
-    print(lgbm_prediction)
-    average = (nn_prediction *0.8 + lgbm_prediction *0.2 ) / 2
+    print('LGBM prediction:', lgbm_prediction)
+    return int(lgbm_prediction)
 
-    return int(average)
+def phish_analyze(email):
+    text = preprocess(email)
+    print(text)
+    phish_text = phish_vectorizer.transform([text])
+    sgd_prediction = sgd_classifier.predict(phish_text) * 100
+    logistic_regression_prediction = logistic_regression_model.predict(phish_text) * 100
+    return int(sgd_prediction)
 
-def preprocess(email_text):
-    email_text = email_text.lower().strip()
-    email_text = re.sub(r"\s+", " ", email_text) 
-    email_text = re.sub(r"[^\w\s]", "", email_text) 
-    email_text = mime_to_text(email_text)  
-    email_text = html_to_text(email_text)  
-    return email_text
+def preprocess(text):
+    if "------=" in text:
+        start_plaintext = text.find("charset=\"Windows-1252\"") + len("charset=\"Windows-1252\"")
+        end_plaintext = text.find("------=", start_plaintext)
+        plaintext_content = text[start_plaintext:end_plaintext].strip()
+
+        #Decode quoted-printable encoding and clean up the content
+        ascii_content = plaintext_content.encode('ascii', 'ignore').decode('ascii')
+        decoded_content = quopri.decodestring(ascii_content).decode('windows-1252')
+        text = decoded_content.replace("=0A=", "\n").replace("=20", " ").replace("=09", "\t")
+
+        #Replace any MIME encoding artifacts
+        text = decoded_content.replace("=0A=", "\n").replace("=20", " ").replace("=09", "\t")
+
+    if "base64" in text.lower():
+        charset_match = re.search(r'charset="([^"]+)"', text)
+        if charset_match:
+            charset = charset_match.group(1)
+        encoded_data = text.strip().split('base64')[-1].strip()
+        try:
+            # Correct the padding for base64 data if necessary
+            padding_needed = len(encoded_data) % 4
+            if padding_needed:  # Padding needed
+                encoded_data += '=' * (4 - padding_needed)
+            decoded_data = base64.b64decode(encoded_data)
+            text = decoded_data.decode(charset if charset else 'iso-8859-1')
+        except Exception as e:
+            print(f"Failed to decode base64: {e}")
+        
+    #Convert html to plain text
+    if '<' in text and '>' in text:
+        soup = BeautifulSoup(text, "html.parser")
+        text = soup.get_text()
+    
+
+    #Convert all text to lowercase
+    text = text.lower()
+    #Removing special whitespace characters
+    text = re.sub(r"\s+", " ", text)
+
+    #Trim whitespaces
+    text = text.strip()
+
+    #Replace phone numbers with placeholder
+    phone_pattern = re.compile(
+        r'(\+?\d{1,2}[-.\s]?)?'       
+        r'(\(?\d{2,4}\)?[-.\s]?)'     
+        r'\d{2,3}[-.\s]?'            
+        r'\d{3,4}'                    
+    )
+  #  text = re.sub(phone_pattern, 'PHONE', text)
+
+    url_pattern = r'\b(?:http|ftp|https)://[^\s()<>]+(?:\([\w\d]+\)|([^[:punct:]\s]|/))'
+    #text = re.sub(url_pattern, 'URL', text)
+
+    return text
 
 if __name__ == "__main__":
     main()
