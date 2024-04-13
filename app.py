9642405678bba3cf72b81329a0da30d525a80432
Anisaftab <102627573+Anisaftab@users.noreply.github.com>
2024-04-13 14:30:15 -0400

    Minor frontend changes

--- app.py
+++ app.py
@@ -32,7 +32,8 @@ nn_tokenizer = joblib.load('nn_tokenizer.joblib')
 
 
 def main():
-    st.title("Email Spam and Phishing Detector")
+    st.title("Spam and Phishing Email Detector")
+    st.set_page_config(page_title="Spam/Phishing Email Prediction App")
     
     #Initialize the email input in session state if it doesn't exist
     if 'email_input' not in st.session_state:
@@ -70,8 +71,8 @@ def main():
             st.write("Phishing Email:", phish_flag)
 
 
-            st.write("Developed by Anis, Terrence, and Noah")
-            st.write("Project under the supervision of Mohamad Hoda")
+    st.write("Developed by Anis, Terrence, and Noah")
+    st.write("Project under the supervision of Mohamad Hoda")
 
 
     
@@ -191,26 +192,3 @@ def preprocess(text):
 
 if __name__ == "__main__":
     main()
-
-
-
-# Load the models
-#    lgbm_model = joblib.load("lgbm_model.pkl")
-#    logistic_regression_model = joblib.load("logisticRegression.pkl")
-#    #nn_model = keras.models.load_model("nn_model.keras")
-#    sgd_classifier_model = joblib.load("sgdClassifier.pkl")
-#    
-#    # Preprocess the email
-#    preprocessed_email = preprocess_email(email)
-#    
-#    # Make predictions using the models
-#    lgbm_prediction = (lgbm_model.predict([preprocessed_email])[0]) * 100
-#    logistic_regression_prediction = (logistic_regression_model.predict([preprocessed_email])[0])* 100
-#    # nn_prediction = (nn_model.predict([preprocessed_email])[0]) * 100
-#    sgd_classifier_prediction = (sgd_classifier_model.predict([preprocessed_email])[0]) * 100
-#    
-#    # Calculate the average spam probability
-#    spam_probability = (lgbm_prediction + logistic_regression_prediction + 0 + sgd_classifier_prediction) / 4
-#    
-#    # Convert spam probability to percentage
-#    spam_percentage = spam_probability 
